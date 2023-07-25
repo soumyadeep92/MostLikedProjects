660a2c658ad515b2577c4404bf9cd4c18e03b3a9
Soumya Deep Das <soumyadeepdas.beas@gmail.com>
2023-07-25 17:06:03 +0530

    loginform SMB node

---
+++ SmallMediumBusiness/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { AuthGuard } from './Components/items/services/guards/auth.guard';
+
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
+import { LogoutComponent } from './Components/logout/logout.component';
+
+/*
+* Routing for the items feature are stored in the items module file
+*/
+
+const routes: Routes = [
+
+    { path: 'dashboard', component: DashboardComponent , canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '',  redirectTo: '/dashboard', pathMatch: 'full' }, // catch all route
+
+];
+export const routingModule: ModuleWithProviders<any> = RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' });
